9cb6397a9f041cc88bf20b5010c38c52bf3cfee8
yuta-akazawa <grief137@gmail.com>
2020-03-30 23:44:33 +0900

    子要素に合わせてContainerの位置を変える

--- prc_link_between_widget/lib/main.dart
+++ prc_link_between_widget/lib/main.dart
@@ -1,79 +1,132 @@
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'drag_model.dart';
-
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => DragModel(),
-      child: MaterialApp(
-        title: 'Link Circles',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: LinkScreen(),
+    return MaterialApp(
+      title: 'Draw Scalable Line',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: LinkScreen(),
     );
   }
 }
 
-class LinkScreen extends StatelessWidget  {
-  DragModelList
+class LinkScreen extends StatefulWidget  {
+  @override
+  LinkState createState() => LinkState();
+}
+
+class LinkState extends State<LinkScreen>  {
+  double x1 = 10.0;
+  double y1 = 10.0;
+  double x2 = 100.0;
+  double y2 = 100.0;
+  double top = 100.0;
+  double left = 100.0;
+  double _preX1, _preY1, _preX2, _preY2;
+  double circleSize = 10.0;
+  double right = 100.0;
+  double bottom = 100.0;
 
   @override
   Widget build(BuildContext context) {
-    final drag1 = Provider.of<DragModel>(context);
-    final drag2 = Provider.of<DragModel>(context);
+//    print("off1: ${Offset(x1, y1)}");
+//    print("off2: ${Offset(x2, y2)}");
+//    print(top);
+    print(left);
 
     return Scaffold(
       body: SafeArea(
         child: Stack(
           children: <Widget>[
-            CustomPaint(
-              painter: LinkPointer(
-                offset1: drag1.offset,
-                offset2: drag2.offset,
+            Positioned(
+              top: top,
+              left: left,
+              right: right,
+              bottom: bottom,
+              child: Container(
+                constraints: BoxConstraints.tight(
+                  Size(
+                      x2 > x1 ? x2 + circleSize : x1 + circleSize,
+                      y2 > y1 ? y2 + circleSize : y1 + circleSize
+                  ),
+                ),
+                color: Colors.green,
+                child: Stack(
+                  overflow: Overflow.visible,
+                  children: <Widget>[
+                    CustomPaint(
+                      painter: LinkPointer(
+                        offset1: Offset(x1, y1),
+                        offset2: Offset(x2, y2),
+                      ),
+                      child: Container(),
+                    ),
+                    Positioned.fromRect(
+                      rect: Rect.fromPoints(
+                          Offset(x1 - 10, y1 - 10),
+                          Offset(x1 + 10, y1 + 10)
+                      ),
+                      child: GestureDetector(
+                        onScaleStart: (details) {
+                          _preX1 = x1;
+                          _preY1 = y1;
+                          print("pre x :${_preX1.toString()}");
+                        },
+                        onScaleUpdate: (details) {
+                          setState(() {
+                            x1 = details.localFocalPoint.dx;
+                            y1 = details.localFocalPoint.dy;
+
+                            print("focalPoint.dx: ${details.focalPoint.dx}");
+//                            print("focalPoint.dy: ${details.focalPoint.dy}");
+                            print("localPoint.dx: ${details.localFocalPoint.dx}");
+//                            print("localPoint.dy: ${details.localFocalPoint.dy}");
+                            double moveX = _preX1 - details.localFocalPoint.dx;
+                            print("moveX: ${moveX.toString()}");
+                            left = details.focalPoint.dx;
+//                            left += moveX;
+
+//                            if (moveX < 0) {
+//                            } else {
+//                              left -= details.localFocalPoint.dx;
+//                            }
+                          });
+                        },
+//                        onScaleEnd: (details) {
+//                          _preX1 = null;
+//                          _preY1 = null;
+//                        },
+                        child: CircleContainer(color: Colors.red),
+                      ),
+                    ),
+                    Positioned.fromRect(
+                      rect: Rect.fromPoints(
+                          Offset(x2 - 10, y2 - 10),
+                          Offset(x2 + 10, y2 + 10)
+                      ),
+                      child: GestureDetector(
+                        onScaleUpdate: (details) {
+                          setState(() {
+                            x2 = details.localFocalPoint.dx;
+                            y2 = details.localFocalPoint.dy;
+                          });
+                        },
+                        child: CircleContainer(color: Colors.blue),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              child: Container(),
-            ),
-            DragWidget(
-              drag: drag1,
-              color: Colors.red,
-            ),
-            DragWidget(
-              drag: drag2,
-              color: Colors.green,
             ),
           ],
         ),
       ),
-    );
-  }
-}
-
-class DragWidget extends StatelessWidget {
-  final DragModel drag;
-  final Color color;
-  DragWidget({this.drag, this.color});
-
-  @override
-  Widget build(BuildContext context) {
-    return Positioned.fromRect(
-      rect: Rect.fromPoints(
-          Offset(drag.dx - 50, drag.dy - 50),
-          Offset(drag.dx + 50, drag.dy + 50)
-      ),
-      child: GestureDetector(
-          onScaleUpdate: (details) {
-            drag.setOffset(details.focalPoint);
-          },
-          child: CircleContainer(color: color),
-        ),
     );
   }
 }
@@ -86,14 +139,12 @@ class CircleContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       transform: Matrix4.identity(),
-      height: 100,
-      width: 100,
       decoration: BoxDecoration(
         shape: BoxShape.circle,
         color: Colors.white,
         border: Border.all(
             color: color,
-            width: 10
+            width: 5
         ),
       ),
     );
@@ -109,7 +160,7 @@ class LinkPointer extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     final Paint paint= Paint()
     ..color = Colors.blueGrey
-    ..strokeWidth = 3;
+    ..strokeWidth = 2;
     canvas.drawLine(offset1, offset2, paint);
   }
 
