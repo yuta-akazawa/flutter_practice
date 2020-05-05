0aadadbe80b37d4ce2c046ced3344dfad35e0959
yuta-akazawa <grief137@gmail.com>
2020-05-06 01:12:42 +0900

    add pan gesture

--- prc_infinite_screen/lib/main.dart
+++ prc_infinite_screen/lib/main.dart
@@ -29,8 +29,15 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   double _scale = 1.0;
   double _previousScale;
-  double x = 100.0;
-  double y = 100.0;
+  double x = 0.0;
+  double y = 0.0;
+  Offset _centerOffset;
+
+  @override
+  void initState() {
+    super.initState();
+    _centerOffset = Offset(x, y);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,37 +50,54 @@ class _MyHomePageState extends State<MyHomePage> {
             child: LayoutBuilder(builder: (context, constraints) {
               final size = Size(constraints.maxWidth, constraints.maxHeight);
               final visibleSize = Size(size.width * 3, size.height * 3);
+//              Offset _centerOffset = Offset(x, y);
+              print("x:${x.toString()}, y:${y.toString()}");
               return Stack(
                 children: <Widget>[
                   Positioned.fromRect(
-                    rect: Rect.fromPoints(
-                        Offset(x - visibleSize.width, y - visibleSize.height),
-                        Offset(x + visibleSize.width, y + visibleSize.height)
+                    rect: Rect.fromCenter(
+                      center: _centerOffset,
+                      width: visibleSize.width,
+                      height: visibleSize.height
                     ),
                     child: GestureDetector(
-                      onScaleStart: (details) {
-                        _previousScale = _scale;
-                      },
-                      onScaleUpdate: (details) {
-                        x = details.focalPoint.dx;
-                        y = details.focalPoint.dy;
-                        print("x:${x.toString()}, y:${y.toString()}");
+//                      onScaleStart: (details) {
+//                        _previousScale = _scale;
+//                      },
+//                      onScaleUpdate: (details) {
+//                        x = details.focalPoint.dx;
+//                        y = details.focalPoint.dy;
+//
+//                        Offset newOffset = _centerOffset.translate(-x, -y);
+//                        setState(() {
+//                          print("_scale:${_scale.toString()}");
+////                          if (_scale < 2.0 && _scale > 0.2) {
+////                          }
+//                            _scale = _previousScale * details.scale;
+//                          _centerOffset = newOffset;
+//                        });
+//                      },
+//                      onScaleEnd: (details) {
+//                        _previousScale = null;
+//                      },
+                      onPanUpdate: (details) {
+                        Offset newOffset = _centerOffset.translate(details.delta.dx, details.delta.dy);
                         setState(() {
-                          _scale = _previousScale * details.scale;
+                          _centerOffset = newOffset;
                         });
                       },
-                      onScaleEnd: (details) {
-                        _previousScale = null;
-                      },
-                      child: Container(
+                      child: Transform(
                         transform: Matrix4.identity()
-                        ..scale(_scale, _scale),
-                        color: Colors.green,
-                        child: Center(
-                          child: Container(
-                            width: 200,
-                            height: 200,
-                            color: Colors.red,
+                          ..scale(_scale, _scale),
+                        alignment: FractionalOffset.center,
+                        child: Container(
+                          color: Colors.green,
+                          child: Center(
+                            child: Container(
+                              width: 200,
+                              height: 200,
+                              color: Colors.red,
+                            ),
                           ),
                         ),
                       ),
